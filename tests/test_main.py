13304c94fa19cfedd65823751b461e1b6841057d
Priyam Jha <priyamj584@gmail.com>
2025-10-23 18:50:55 +0530

    added new funcion and test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
     assert add(-2, -3) == -5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
+    assert subtract(-3, -2) == -1
